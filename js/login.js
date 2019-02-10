bd378a6749313dfa1656ed7859825caea90caef6
fatihemree <fatihemree.klm@gmail.com>
2019-02-10 13:40:22 +0300

    login card tasarımını bitmiştir ajax bağlantısı kaldı

--- js/login.js
+++ js/login.js
@@ -2,28 +2,30 @@
 $(document).ready(function(){
   $('.datepicker').datepicker();
 });
-
+//open
 $("#tikla").click(function() {
-
   $(".login-block").toggleClass("animated fadeInDown").
   css("display","block")
   .toggleClass("animated fadeInDown","slow");
-
-
-
-
 });
-
+//close
 $("#close").click(function() {
- 
  $(".login-block").toggleClass("animated zoomOut")
- .toggleClass("animated zoomOut","slow",function(){ $(".login-block").hide()});
- 
-
- 
+ .toggleClass("animated zoomOut","slow",function(){
+    $(".login-block").hide();
+    $(".loginUsers").show();
+    $(".signUsers").hide();
+});
 } );
-
-
+//sig in
+$(".login_signButton").click(function() {
+  $( ".loginUsers" ).hide( "slide", { direction: "down" }, "slow",function(){ $(".signUsers").show("slide", { direction: "up" },"slow");     } );
+ } );
+ //sigin to login button
+ $(".users-loginButton span").
+ click(function(){ 
+  $( ".signUsers" ).hide( "slide", { direction: "up" }, "slow",function(){ $(".loginUsers").show("slide", { direction: "down" },"slow");     } );
+       });
 
 
 
